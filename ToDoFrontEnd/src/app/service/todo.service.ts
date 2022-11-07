dec2065f520ab98dd31a9a6db16606d922f43006
Jie Li <JLi147@slb.com>
2022-11-07 21:46:46 +0800

    feat: add creating todoItem test

--- ToDoFrontEnd/src/app/service/todo.service.ts
+++ ToDoFrontEnd/src/app/service/todo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { TodoApiService } from '../api/todo.api.service';
 import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 
@@ -9,7 +10,8 @@ export class TodoService {
 
   private _selectedTodoItem: ToDoItem = {} as ToDoItem;
   private _updatingTodoItem: ToDoItem = {} as ToDoItem;
-  constructor(private todoStore: TodoStoreService) {
+  constructor(private todoStore: TodoStoreService,
+              private todoApi: TodoApiService) {
   }
 
   public get todoItems(): Array<ToDoItem> {
@@ -21,7 +23,7 @@ export class TodoService {
   }
 
   public create(todoItem: ToDoItem): void {
-    this.todoStore.create(todoItem);
+    this.todoApi.create(todoItem);
   }
 
   public update(updateTodoItem: ToDoItem): void {
@@ -38,13 +40,5 @@ export class TodoService {
 
   public selectTodoItemForUpdate(id: number): void {
     this._updatingTodoItem = Object.assign({}, this.todoStore.findById(id));
-  }
-
-  public currentTodoItem(): ToDoItem {
-    return this._selectedTodoItem;
-  }
-
-  public currentUpdatingTodoItem(): ToDoItem {
-    return this._updatingTodoItem;
   }
 }
